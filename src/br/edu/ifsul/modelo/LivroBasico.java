ae28eed90b2019f949dabc0e70440c4c99c5c520
ROBSON <ROBSON@ROBSON-PC>
2016-09-07 22:32:10 -0300

    comit 2 do projeto

---
+++ src/br/edu/ifsul/modelo/LivroBasico.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.modelo;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ *
+ * @author ROBSON
+ */
+@Entity
+@Table(name = "livro_basico")
+public class LivroBasico implements Serializable{
+    @Id
+    @NotNull(message = "O ISBN não pode ser nulo")
+    @Length(max = 13, message = "O ISBN não pode ter mais de {max} caracteres")
+    @NotBlank(message = "O ISBN não pode ser em branco")
+    @Column(name = "ISBN", length = 13, nullable = false)
+    private String ISBN;
+    @NotNull(message = "O Titulo não pode ser nulo")
+    @Length(max = 50, message = "O Titulo não pode ter mais de {max} caracteres")
+    @NotBlank(message = "O Titulo não pode ser em branco")
+    @Column(name = "titulo", length = 50, nullable = false)
+    private String titulo;
+    @Column(name = "resumo", columnDefinition = "text")
+    private String resumo;
+    @NotNull(message = "A editora não pode ser nula")
+    @Length(max = 30, message = "A editora não pode ter mais de {max} caracteres")
+    @NotBlank(message = "A editora não pode ser em branco")
+    @Column(name = "editora", length = 30, nullable = false)
+    private String editora;
+    @Temporal(TemporalType.TIMESTAMP)
+    @NotNull(message = "A data deve ser informada")
+    @Column(name = "data_publicacao", nullable = false)
+    private Calendar dataPublicacao;
+
+    public LivroBasico() {
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public Calendar getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(Calendar dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.ISBN);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LivroBasico other = (LivroBasico) obj;
+        if (!Objects.equals(this.ISBN, other.ISBN)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
--- test/br/edu/ifsul/testes/junit/TestePersistirLivroBasico.java
+++ test/br/edu/ifsul/testes/junit/TestePersistirLivroBasico.java
@@ -6,20 +6,24 @@
 package br.edu.ifsul.testes.junit;
 
 import br.edu.ifsul.jpa.EntityManagerUtil;
-import br.edu.ifsul.modelo.Autor;
+import br.edu.ifsul.modelo.LivroBasico;
+import java.util.Calendar;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import junit.framework.Assert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
  * @author ROBSON
  */
-public class TestePersistirAutor {
+public class TestePersistirLivroBasico {
     EntityManager em;
-    public TestePersistirAutor() {
+    public TestePersistirLivroBasico() {
     }
     
     @Before
@@ -36,11 +40,14 @@ public class TestePersistirAutor {
     public void teste(){
         boolean exception = false;
         try {
-            Autor a = new Autor();
-            a.setNome("Afonso");
-            a.setBibliografia("Nasceu em 1975, doutourad em");
+            LivroBasico lb = new LivroBasico();
+            lb.setISBN("1234567891234");
+            lb.setTitulo("Persistencia testes");
+            lb.setResumo("Testanndo persistencia com JPA");
+            lb.setEditora("Nuova");
+            lb.setDataPublicacao(Calendar.getInstance());
             em.getTransaction().begin();
-            em.persist(a);
+            em.persist(lb);
             em.getTransaction().commit();
         } catch(Exception e){
             exception = true;
